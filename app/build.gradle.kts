89c098912a9805e6a93810610d2d4a20de54e2b5
BennyJCM <bennycm.13@gmail.com>
2025-05-10 11:16:30 -0500

    Se añade navegación a la app con Compose Navigation. - Se crea el PantallaMenu.kt para la navegación principal. - Se crea el CatalogoProductosActivity.kt, CalculadoraEdadScreen.kt, ConversorDivisasScreen.kt. - Se añade librerías de Navigation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,4 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.5.0")
     //Add navigation compose
     implementation("androidx.navigation:navigation-compose:2.6.0")
-    implementation("io.coil-kt:coil-compose:2.0.0")
 }
